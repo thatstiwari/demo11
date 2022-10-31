53f3d835c919044a3f760b299cb555b42b4a5262
thatstiwari <thatstiwari@gmail.com>
2022-10-31 13:41:23 +0530

    ok

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-	System.out.println("Welcome to the java World");
-	System.out.println("First edit after jenkins integration");
+	System.out.println("Welcome to the jenkins World");
+	System.out.println("Lab question after  jenkins integration");
 }
 }
